59220b26640e25c75be0f2b41802e09d0e2c1bc5
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-07-30 18:04:36 +0900

    write readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# youtuve Reloaded
+
+/ -> Home
+/join -> join
+/login -> Login
+/search -> Search
+
+/users -> users
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> comment on a video
+/videos/comments/delete -> Delete A Comment of a video
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import logger from "morgan";
 
 const PORT = 4000
 const app = express();
+const loggerMiddleware = logger("dev")
 
 const handleHome = (req, res) => {
     return res.end("I still love you");
@@ -10,6 +12,7 @@ const handleLogin = (req, res) => {
     return res.send("Login here.");
 }
 
+app.use(loggerMiddleware);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
