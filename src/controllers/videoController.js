e6ffd4056e9ea449a8540e2eaaf0ed42c7b0e30c
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-08-04 20:32:59 +0900

    fakeDB recap

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,11 +39,26 @@ export const getEdit = (req, res) => {
     const video = videos[id-1];
     return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
 }
-
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
     videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
 
+}
+export const getUpload = (req, res) => {
+    return res.render("upload");
+}
+export const postUpload = (req, res) => {
+    const { title } = req.body
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createdAt: "just now",
+        views: 0,
+        id: videos.length+1,
+    }
+    videos.push(newVideo);
+    return res.redirect('/');
 }
