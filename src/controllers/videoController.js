ad2ca27e96a075d8803e34be1909cbd06706db83
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-08-08 19:20:29 +0900

    #6.20

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,25 +8,42 @@ export const home = async (req, res) => {
     catch(error){
         return res.render("ERROR", error);
     }
-  };
-export const watch = (req, res) =>  {
-    const { id }  = req.params;
-    return res.render("watch", {pageTitle: `Watching`});
 }
-export const getEdit = (req, res) => {
+export const watch = async (req, res) =>  {
     const { id }  = req.params;
-    return res.render("edit", {pageTitle: `Editing`});
+    const video = await Video.findById(id);
+    if (video) {
+        return res.render("watch", {pageTitle: video.title, video});
+    }
+    return res.render("404", {pageTitle: "Video not found."});
+}
+export const getEdit = async (req, res) => {
+    const { id }  = req.params;
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("edit", {pageTitle: `Editing`, video});
 }
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
     return res.redirect(`/videos/${id}`);
-
 }
 export const getUpload = (req, res) => {
     return res.render("upload");
 }
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const { title, description, hashtags } = req.body;
-    returnbres.redirect('/');
+    try{
+        await Video.create({
+            title,
+            description,
+            hashtags: hashtags.split(",").map((word) => `#${word}`),
+        });
+        return res.redirect('/');
+    }
+    catch (error) {
+        return res.render("upload", {pageTitle: "Upload Video", errorMessage: error._message});
+    }
 }
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const loggerMiddleware = logger("dev")
 app.set("view engine", "pug")
 app.set("views", process.cwd() + "/src/views");
 app.use(loggerMiddleware);
-app.use(express.urlencoded({ extended: true} ));
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
